2ccc3242dc9b21ea4e70b4ff86745c4360c36e8b
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-08 12:43:45 +0100

    Settaggio variabile JS

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+//Andiamo a selezionare il nostro output
+const containerOutput = document.getElementById("container");
+
+//Andiamo a creare una variabile endpoint dove inserire il link
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+
